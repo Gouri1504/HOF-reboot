252af391152be5ec4ff149c7ef8b71e304fe11f5
Gouri Agarwal <gouriagarwal15@gmail.com>
2024-03-01 15:22:49 +0530

    Done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HigherOrderComponent from './Components/HigherOrderComponent.jsx';
+
+function App() {
+  return (
+   <>
+   <HigherOrderComponent />
+   </>
+  );
+}
+
+export default App;
